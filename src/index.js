b56d4f02ec211f54860e8f87c4e1ba2052bee623
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-09-18 11:08:00 +0530

    🎉 setup redux store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { store } from "./store";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
