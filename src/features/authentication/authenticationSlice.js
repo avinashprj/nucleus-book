72030b2ff3b566fc394f6776567372dd2700f870
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-09-22 00:18:04 +0530

    ✨add auth slice

---
+++ src/features/authentication/authenticationSlice.js
@@ -0,0 +1,80 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import { postLoginData, postSignupData } from "../../serverCalls";
+
+export const loginUser = createAsyncThunk(
+  "authenticate/loginUser",
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const loginResponse = await postLoginData(username, password);
+      return loginResponse.data;
+    } catch (error) {
+      toast.error(`Incorrect username or password`);
+      console.error(error.response.data);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+export const signupUser = createAsyncThunk(
+  "authenticate/signupUser",
+  async (userDetails, { rejectWithValue }) => {
+    try {
+      const signupResponse = await postSignupData(userDetails);
+      return signupResponse.data;
+    } catch (error) {
+      toast.error(`Couldn't Signup! Please try again.`);
+      console.error(error.response.data);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+const initialState = {
+  authToken: localStorage.getItem("authToken") ?? "",
+  authUser: JSON.parse(localStorage.getItem("authUser")) ?? {},
+  authStatus: "idle",
+  authError: null,
+};
+
+const authenticationSlice = createSlice({
+  name: "authentication",
+  initialState,
+  reducers: {
+    logoutUser: (state) => {
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("authUser");
+      state.authToken = null;
+      state.authUser = null;
+      state.authStatus = "idle";
+      state.authError = null;
+      toast.success("Logout successful");
+    },
+  },
+  extraReducers: {
+    [loginUser.pending]: (state) => {
+      state.authStatus = "pending";
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      state.authStatus = "fulfilled";
+      state.authToken = action.payload.encodedToken;
+      state.authUser = action.payload.foundUser;
+      localStorage.setItem("authToken", state.authToken);
+      localStorage.setItem("authUser", JSON.stringify(state.authUser));
+    },
+    [loginUser.rejected]: (state, action) => {
+      state.authStatus = "Error";
+      state.authError = action.payload;
+    },
+    [signupUser.pending]: (state) => {
+      state.authStatus = "pending";
+    },
+    [signupUser.fulfilled]: (state, action) => {
+      state.authStatus = "fulfilled";
+      state.authToken = action.payload.encodedToken;
+      state.authUser = action.payload.createdUser;
+      localStorage.setItem("authToken", state.authToken);
+      localStorage.setItem("authUser", JSON.stringify(state.authUser));
+    },
+  },
+});
+export const authenticationReducer = authenticationSlice.reducer;
+export const { logoutUser } = authenticationSlice.actions;
