2494e57f499a87dfaa4c6d0070fd5c800e80548b
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-09-22 00:06:03 +0530

    🔧 fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: "#1d4ed8",
-      white: "#FFF",
-    },
     fontFamily: {
       sen: ["Sen", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1d4ed8",
+        white: "#FFF",
+      },
+    },
   },
 
   plugins: [],
