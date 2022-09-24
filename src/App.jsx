d4ad3ccfb888324696b3dd52d26d509faa00e9ee
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-09-24 20:16:42 +0530

    ✨add toastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { Login, Signup } from "./features";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   console.log("EHLLO");
@@ -7,9 +9,26 @@ function App() {
     <div className="App">
       <Routes>
         {/* Temporary path for login */}
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <h1>
+              HELLO WORLD{" "}
+              <strong>
+                <Link to="/login">login</Link>
+              </strong>
+            </h1>
+          }
+        />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+      <ToastContainer
+        theme="light"
+        position="bottom-right"
+        autoClose={800}
+        draggable
+      />
     </div>
   );
 }
