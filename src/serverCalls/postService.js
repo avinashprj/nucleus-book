49c6673e26f2b7c531374360f8f6780a4e84a27e
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-12-04 00:44:58 +0530

    feat: 🎸 add post server calls

---
+++ src/serverCalls/postService.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const getAllPostsFromServer = () => axios.get("/api/posts");
+
+const getPagedPostsFromServer = (pageNum) =>
+  axios.get(`/api/posts/page/${pageNum}`);
+
+const getAllPostsOfUserFromServer = (username) =>
+  axios.get(`/api/posts/user/${username}`);
+
+const addPostToServer = (postData, authorization) =>
+  axios.post("/api/posts", { postData }, { headers: { authorization } });
+
+const editPostInServer = (postData, authorization) =>
+  axios.post(
+    `/api/posts/edit/${postData._id}`,
+    { postData },
+    { headers: { authorization } }
+  );
+
+const deletePostFromServer = (postId, authorization) =>
+  axios.delete(`/api/posts/${postId}`, { headers: { authorization } });
+
+const likePostInServer = (postId, authorization) =>
+  axios.post(`/api/posts/like/${postId}`, {}, { headers: { authorization } });
+
+const dislikePostInServer = (postId, authorization) =>
+  axios.post(
+    `/api/posts/dislike/${postId}`,
+    {},
+    {
+      headers: { authorization },
+    }
+  );
+
+export {
+  getAllPostsFromServer,
+  getAllPostsOfUserFromServer,
+  addPostToServer,
+  editPostInServer,
+  deletePostFromServer,
+  likePostInServer,
+  dislikePostInServer,
+  getPagedPostsFromServer,
+};
