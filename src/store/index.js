18ea31c2e1be3774db0c61b7e603f95f4da85bda
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-12-03 23:47:01 +0530

    feat: 🎸 add all the reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authenticationReducer } from "../features/authentication/authenticationSlice";
-import { usersReducer } from "../features/main/usersSlice";
+import { usersReducer } from "../features/home/usersSlice";
+import { postsReducer } from "../features/home/postsSlice";
+import { profileReducer } from "../features/profile/profileSlice";
 
 export const store = configureStore({
   reducer: {
     authentication: authenticationReducer,
     users: usersReducer,
+    profile: profileReducer,
+    posts: postsReducer,
   },
 });
