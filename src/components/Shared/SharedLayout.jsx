42e0b1668b68798d7f5616410bd1ffbc8de51be5
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-12-04 01:13:18 +0530

    feat: 🎸 add shared router

---
+++ src/components/Shared/SharedLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../Navbar/Navbar";
+import { Footer } from "../Footer/Footer";
+
+export const SharedLayout = () => (
+  <>
+    <header>{/* <Navbar /> */}</header>
+    <Outlet />
+    {/* <Footer /> */}
+  </>
+);
