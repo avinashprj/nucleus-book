0820668b6ad54d285b96b5d40233de91b687c500
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-12-04 01:14:54 +0530

    refactor: 💡 add outlet to private route

--- src/features/authentication/components/PrivateRoute.jsx
+++ src/features/authentication/components/PrivateRoute.jsx
@@ -1,11 +1,11 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function PrivateRoute({ children }) {
   const { authToken } = useSelector((state) => state.authentication);
   const location = useLocation();
   return authToken ? (
-    children
+    <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
   );
