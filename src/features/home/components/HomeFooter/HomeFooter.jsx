89211f5fa5a339973cbb4366cfcfd1ca6798bd29
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-12-04 01:21:02 +0530

    feat: 🎸 add main page footer

---
+++ src/features/home/components/HomeFooter/HomeFooter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const HomeFooter = () => (
+  <footer>
+    <ul className="text-xs text-gray-500 my-4 mx-2">
+      <li className="inline-block mx-2">
+        <a className="hover:underline" href="/">
+          Terms of Service
+        </a>
+      </li>
+      <li className="inline-block mx-2">
+        <a className="hover:underline" href="/">
+          Privacy Policy
+        </a>
+      </li>
+      <li className="inline-block mx-2">
+        <a className="hover:underline" href="/">
+          Cookie Policy
+        </a>
+      </li>
+      <li className="inline-block mx-2">
+        <a className="hover:underline" href="/">
+          Ads info
+        </a>
+      </li>
+      <li className="inline-block mx-2">
+        <a className="hover:underline" href="/">
+          More
+        </a>
+      </li>
+      <li className="inline-block mx-2">© 2020 Twitter, Inc.</li>
+    </ul>
+  </footer>
+);
